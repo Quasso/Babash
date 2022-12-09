80f70f457812c320e402361e1a81b693b52a41d2
Quasso <alex@forbettergood.ge>
2022-12-09 23:09:55 +0000

    added prefix bb_ to all functions (as a kind of namespace, though it could possible clash something else...) okay might begin to 'modularise' this now while it's tame, and come up with a viable strategy for naming/imports etc...

--- src/babash.sh
+++ src/babash.sh
@@ -12,26 +12,26 @@
 # which could have benefits but also restricts where
 # this can be imported.
 
-function debug_echo() {
+function bb_debug_echo() {
     OUTPUT=$1
     if [[ $DEBUG_LOGS_ENABLED == true ]]; then
         echo "DEBUG: $OUTPUT"
     fi
 }
 
-function check_is_var_set() {
+function bb_check_is_var_set() {
     VAR=$1
     DEFAULT_VAL=$2
 
     if [[ -z ${VAR+x} || ${VAR} == '' ]]; then
-        debug_echo "var is unset, setting now to $DEFAULT_VAL"
+        bb_debug_echo "var is unset, setting now to $DEFAULT_VAL"
         $VAR=$DEFAULT_VAL
     else
-        debug_echo "var is set to '$VAR'"
+        bb_debug_echo "var is set to '$VAR'"
     fi
 }
 
-function multiline_echo() {
+function bb_multiline_echo() {
     COUNT=$1
 
     for i in $(seq 1 $COUNT); do
@@ -40,15 +40,19 @@ function multiline_echo() {
 }
 
 DEFAULT_VAL_PADDING_LINES=10
-function padded_output() {
+function bb_padded_output() {
     CONTENT=$1
     PADDING_LINES=$2
 
-    check_is_var_set $PADDING_LINES $DEFAULT_VAL_PADDING_LINES
+    bb_check_is_var_set $PADDING_LINES $DEFAULT_VAL_PADDING_LINES
 
-    multiline_echo $PADDING_LINES
+    bb_multiline_echo $PADDING_LINES
 
     echo $CONTENT
 
-    multiline_echo $PADDING_LINES
+    bb_multiline_echo $PADDING_LINES
+}
+
+function bb_sm_pad() {
+
 }
